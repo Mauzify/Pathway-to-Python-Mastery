b6d584470dbc04a976a64e6aee61a05c26e799c7
Mauzify <jacobmnagel@gmail.com>
2026-04-18 11:02:46 -0500

    UPDATED: PasswordGenerator.py

--- PasswordGenerator.py
+++ PasswordGenerator.py
@@ -4,18 +4,28 @@ import secrets
 import string
 # string is a built in python library that provides a collection of string constants, such as ascii letters, digits and punctuation,
 # which can be used to create complex passwords.
+import math 
+# Do I even need to explain this ;-;
 
 def generate_the_damn_password(length, use_digits, use_special): # the function that tells the program how to actually generate the password .
     chars = string.ascii_lowercase # starts with lower case letters as base.
-
     if use_digits: # if using digits is true, add digits to the characters pool.
         chars += string.digits
     if use_special: # if using special characters is true, add punctuation and uppercase letters to the characters pool, case sensitive. 
         chars += string.punctuation
     chars += string.ascii_uppercase # adds the actual uppercase letters to the characters pool. 
 
-    password = ''.join(secrets.choice(chars) for _ in range(length)) # generates a password by randomly inputting numbers from the characters pool, hense the for _ in range
-    return password # stop the function and return what has been generated.
+    pool_size = len(chars)
+    password = ''.join(secrets.choice(chars) for _ in range(length))
+    return password, pool_size
+
+def check_strength_of_password(length, pool_size):
+    entropy = length * math.log2(pool_size)
+    percentage = (entropy / 128) * 100
+    
+    if percentage >= 100:
+        percentage = 100
+    return round(percentage, 1)
 
 def program(): # program function that runs the actual UI.
     print("--- PASSWORD GENERATOR ---")
@@ -38,10 +48,12 @@ def program(): # program function that runs the actual UI.
         elif digits and special:
             print("NOTICE: Your password is set to HARDENED MODE. Digits and symbols are included. This is the most secure form of password you can generate with this program.") # if both digits and special are true, print this notice.
 
-        new_password = generate_the_damn_password(length, digits, special) # calls password function and stores the result in new_password variable.
+        new_password, pool_size = generate_the_damn_password(length, digits, special) 
+        strength_score = check_strength_of_password(length, pool_size) 
 
         print("\n" + "="*30) # prints a line of 30 equal signs for visual separation.
         print(f"Generated Password: {new_password}")
+        print(f"Password Strength: {strength_score}%")
         print("="*30) # prints a line of 30 equal signs for visual separation.
         print("Note: Never share this password in plain text.")
 
